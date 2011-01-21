ddf5d4caa7962d39cc18c4591f3837573b17225d
chris <cool45u@hotmail.com>
2011-01-21 11:47:24 -0500

    add list of messages to user show page and order messages by most recent first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   # GET /users/1.xml
   def show
     @user = User.find(params[:id])
+	@messages =@user.messages
 
     respond_to do |format|
       format.html # show.html.erb
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
 	validates_presence_of :name
-	has_many :messages
+	has_many :messages, :order => "created_at Desc"
 end
